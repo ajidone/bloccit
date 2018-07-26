f1ffd2a50b1e216a6831ed3111a454ff029d8d9d
Adam Idone <ajidone@gmail.com>
2018-07-25 19:30:03 -0700

    assignment-16-seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'random_data'
 
+post = Post.find_or_create_by!(title: "This is the first test post 2018!", body: "This is it's body")
+Comment.find_or_create_by!(post: post, body: "This is a test comment 2018!")
 50.times do
   Post.create!(
     title: RandomData.random_sentence,
